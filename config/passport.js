f7d3c1ef73ab7c00547934603db5505249406c82
Caesar Bell <caesar.jd.bell@gmail.com>
2018-11-10 15:59:51 -0500

    auth using passport

--- config/passport.js
+++ config/passport.js
@@ -14,10 +14,9 @@ opts.secretOrKey = keys.secretOrKey;
 
  module.exports = (passport) => {
      passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        console.log(jwt_payload); 
+        
         User.findById(jwt_payload.id)
             .then(user => {
-                console.log('user', user)
                 if(user) {
                     return done(null, user); 
                 }
